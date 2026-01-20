4379e2572f023e29c7331cbe6691abf872dd5b3b
Ritesh2004-0 <riteshadling159@gmail.com>
2026-01-20 20:30:44 +0530

    Add files via upload

---
+++ Face_Eye_Detection.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+import os
+os.chdir(r"C:\ritesh\PYTHON\MINI PROJECT")
+
+# Load emotion model
+model = load_model("emotion_model.hdf5",compile=False)
+
+# Emotion labels
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+# Load face detector
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_gray = cv2.resize(roi_gray, (64, 64))
+        roi_gray = roi_gray / 255.0
+        roi_gray = np.reshape(roi_gray, (1, 64, 64, 1))
+
+        prediction = model.predict(roi_gray)
+        emotion = emotion_labels[np.argmax(prediction)]
+
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.putText(frame, emotion, (x, y-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+
+    cv2.imshow("Emotion Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
